5744a379089e49c6f5a844f70cce3d25e389b424
Embedthis Development <dev@embedthis.com>
2025-11-03 07:28:34 +1100

    TEST: CICD

--- test/setup.sh
+++ test/setup.sh
@@ -6,11 +6,9 @@
 set -m
 
 set -x
-echo "@@ Starting GoAhead test server on port localhost:18080"
-which goahead-test
 
-if curl -s http://localhost:18080 / >/dev/null 2>&1; then
-    echo "GoAhead is already running on port localhost:18080"
+if curl -s http://127.0.0.1:18080 > /dev/null 2>&1; then
+    echo "GoAhead is already running on port 18080"
     sleep 999999 &
 else
     goahead-test -v &
